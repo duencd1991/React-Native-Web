bb0cf775d27ee9cc8b448f4fdf3608634b00a795
Nguyen Canh Duc Due <Due.NguyenCanhDuc@thanglongsc.com.vn>
2019-02-22 14:31:26 +0700

    Init project

---
+++ App.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import App from './App';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
